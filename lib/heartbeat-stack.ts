9e60627ed58713ffd53395961aabc60e7a94c9b6
Joe Pollard <joeslice@gmail.com>
2020-11-16 11:01:37 -0600

    Lambda integration

--- lib/heartbeat-stack.ts
+++ lib/heartbeat-stack.ts
@@ -1,9 +1,27 @@
 import * as cdk from '@aws-cdk/core';
+import { HttpApi } from '@aws-cdk/aws-apigatewayv2';
+import { LambdaProxyIntegration } from '@aws-cdk/aws-apigatewayv2-integrations';
+import * as lambda from '@aws-cdk/aws-lambda';
 
 export class HeartbeatStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const checkin = new lambda.Function(this, 'checkinlambda', {
+      code: lambda.Code.fromAsset('lambda'),
+      handler: 'heartbeat.checkin',
+      runtime: lambda.Runtime.NODEJS_10_X
+    });
+
+    const api = new HttpApi(this, 'api', {
+      apiName: 'heartbeat'
+    });
+
+    api.addRoutes({
+      path: "/checkin",
+      integration: new LambdaProxyIntegration({
+        handler: checkin
+      })
+    })
   }
 }
