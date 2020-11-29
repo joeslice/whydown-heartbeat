85d583100bfc437e1775dc4a47a10fb0a20e12ae
Joe Pollard <joeslice@gmail.com>
2020-11-29 07:27:06 -0600

    Include x-ray tracing

--- lambda/heartbeat.js
+++ lambda/heartbeat.js
@@ -1,4 +1,5 @@
-const AWS = require('aws-sdk');
+const AWSXRay = require('aws-xray-sdk-core')
+const AWS = AWSXRay.captureAWS(require('aws-sdk'))
 const db = new AWS.DynamoDB.DocumentClient();
 
 const tableName = process.env.TABLE_NAME;
