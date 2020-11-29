a3582a8dc3b7f1824d5084ec5cfe5a77b04db247
Joe Pollard <joeslice@gmail.com>
2020-11-29 06:58:46 -0600

    Add checkin endpoint to update dynamo

---
+++ lambda/heartbeat.js
@@ -0,0 +1,53 @@
+const AWS = require('aws-sdk');
+const db = new AWS.DynamoDB.DocumentClient();
+
+const tableName = process.env.TABLE_NAME;
+
+exports.checkin = async function(event) {
+    console.log("table", tableName, "request", JSON.stringify(event, undefined, 2));
+    const { queryStringParameters, pathParameters } = event;
+    
+    if (queryStringParameters['pingId'] && pathParameters['reporter']) {
+        const { pingId } = queryStringParameters;
+        const { reporter } = pathParameters;
+
+        const params = {
+            TableName: tableName,
+            Key: {
+                reporter: reporter
+            },
+            UpdateExpression: `set lastPing = :pingId , checkinTime = :checkinTime`,
+            ExpressionAttributeValues: {
+                ':pingId': pingId,
+                ':checkinTime': Date.now()
+            }
+        };
+        
+        try {
+            await db.update(params, function (err, data) {
+                if (err) console.log(err, params);
+            }).promise();
+            return { statusCode: 204, body: '' };
+        }
+        catch (dbError) {
+            console.log("ERROR:", JSON.stringify(dbError));
+            return { statusCode: 500 };
+        }
+    } else {
+        return {
+            statusCode: 400
+        }
+    }
+};
+
+exports.back = async function(event) {
+    console.log("request", JSON.stringify(event, undefined, 2));
+
+    const { protocol, sourceIp, userAgent } = event.requestContext.http;
+
+    return {
+        statusCode: 200,
+        headers: { "Content-Type": "text/plain" },
+        body: `Back recorded ${protocol} to ${userAgent} from ${sourceIp}\n`
+    }
+};
--- lib/heartbeat-stack.ts
+++ lib/heartbeat-stack.ts
@@ -2,26 +2,48 @@ import * as cdk from '@aws-cdk/core';
 import { HttpApi } from '@aws-cdk/aws-apigatewayv2';
 import { LambdaProxyIntegration } from '@aws-cdk/aws-apigatewayv2-integrations';
 import * as lambda from '@aws-cdk/aws-lambda';
+import * as dynamodb from '@aws-cdk/aws-dynamodb';
+
+const PARTITION_KEY = 'reporter';
+const SORT_KEY = 'checkinTime';
 
 export class HeartbeatStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const checkin = new lambda.Function(this, 'checkinlambda', {
-      code: lambda.Code.fromAsset('lambda'),
-      handler: 'heartbeat.checkin',
-      runtime: lambda.Runtime.NODEJS_10_X
+    const table = new dynamodb.Table(this, 'CheckinTable', {
+      partitionKey: {
+        name: PARTITION_KEY,
+        type: dynamodb.AttributeType.STRING
+      },
+      billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      tableName: 'checkins'
     });
 
-    const api = new HttpApi(this, 'api', {
+    const checkin = new lambda.Function(this, 'CheckinLambda', {
+      code: lambda.Code.fromAsset('lambda'),
+      handler: 'heartbeat.checkin',
+      runtime: lambda.Runtime.NODEJS_10_X,
+      environment: {
+        TABLE_NAME: table.tableName,
+        PARTITION_KEY: PARTITION_KEY,
+        SORT_KEY: SORT_KEY
+      },
+      tracing: lambda.Tracing.ACTIVE
+    });
+
+    table.grantWriteData(checkin);
+
+    const api = new HttpApi(this, 'Api', {
       apiName: 'heartbeat'
     });
 
     api.addRoutes({
-      path: "/checkin",
+      path: "/{reporter}/checkin",
       integration: new LambdaProxyIntegration({
         handler: checkin
       })
-    })
+    });
   }
 }
