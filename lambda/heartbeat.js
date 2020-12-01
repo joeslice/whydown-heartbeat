22f8994c9d6ef8ae1727e17bfbe8fde3f17a8809
Joe Pollard <joeslice@gmail.com>
2020-12-01 16:55:34 -0600

    Add back endpoint

--- lambda/heartbeat.js
+++ lambda/heartbeat.js
@@ -2,53 +2,57 @@ const AWSXRay = require('aws-xray-sdk-core')
 const AWS = AWSXRay.captureAWS(require('aws-sdk'))
 const db = new AWS.DynamoDB.DocumentClient();
 
-const tableName = process.env.TABLE_NAME;
+const checkinTable = process.env.CHECKIN_TABLE_NAME;
+const backTable = process.env.BACK_TABLE_NAME;
 
 exports.checkin = async function(event) {
-    console.log("table", tableName, "request", JSON.stringify(event, undefined, 2));
+    console.log("request", JSON.stringify(event, undefined, 2));
     const { queryStringParameters, pathParameters } = event;
     
     if (queryStringParameters['pingId'] && pathParameters['reporter']) {
         const { pingId } = queryStringParameters;
         const { reporter } = pathParameters;
 
-        const params = {
-            TableName: tableName,
+        const updateParams = {
+            TableName: checkinTable,
             Key: {
                 reporter: reporter
             },
             UpdateExpression: `set lastPing = :pingId , checkinTime = :checkinTime`,
             ExpressionAttributeValues: {
                 ':pingId': pingId,
-                ':checkinTime': Date.now()
+                ':checkinTime': Date.now()/1000
             }
         };
         
-        try {
-            await db.update(params, function (err, data) {
-                if (err) console.log(err, params);
-            }).promise();
-            return { statusCode: 204, body: '' };
-        }
-        catch (dbError) {
-            console.log("ERROR:", JSON.stringify(dbError));
-            return { statusCode: 500 };
-        }
+        await db.update(updateParams, function (err, data) {
+            if (err) console.log(err, updateParams);
+        }).promise();
+        return { statusCode: 204, body: '' };
     } else {
-        return {
-            statusCode: 400
-        }
+        return { statusCode: 400 }
     }
 };
 
 exports.back = async function(event) {
     console.log("request", JSON.stringify(event, undefined, 2));
+    const { queryStringParameters, pathParameters } = event;
 
-    const { protocol, sourceIp, userAgent } = event.requestContext.http;
+    if (pathParameters['reporter']) {
+        const { reporter } = pathParameters;
+        const { outage, missed } = queryStringParameters;
 
-    return {
-        statusCode: 200,
-        headers: { "Content-Type": "text/plain" },
-        body: `Back recorded ${protocol} to ${userAgent} from ${sourceIp}\n`
+        await db.put({
+            TableName: backTable,
+            Item: {
+                reporter: reporter,
+                checkinTime: Date.now()/1000,
+                outage: outage,
+                missed: missed
+            }
+        }).promise();
+        return { statusCode: 204 };
+    } else {
+        return { statusCode: 400 };
     }
 };
--- lib/heartbeat-stack.ts
+++ lib/heartbeat-stack.ts
@@ -11,7 +11,7 @@ export class HeartbeatStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const table = new dynamodb.Table(this, 'CheckinTable', {
+    const checkinTable = new dynamodb.Table(this, 'CheckinTable', {
       partitionKey: {
         name: PARTITION_KEY,
         type: dynamodb.AttributeType.STRING
@@ -21,19 +21,48 @@ export class HeartbeatStack extends cdk.Stack {
       tableName: 'checkins'
     });
 
-    const checkin = new lambda.Function(this, 'CheckinLambda', {
-      code: lambda.Code.fromAsset('lambda'),
+    const backTable = new dynamodb.Table(this, 'BackTable', {
+      partitionKey: {
+        name: PARTITION_KEY,
+        type: dynamodb.AttributeType.STRING
+      },
+      sortKey: {
+        name: SORT_KEY,
+        type: dynamodb.AttributeType.NUMBER
+      },
+      billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      tableName: 'back'
+    });
+
+    const lambdaEnvironment = {
+      CHECKIN_TABLE_NAME: checkinTable.tableName,
+      BACK_TABLE_NAME: backTable.tableName,
+      PARTITION_KEY: PARTITION_KEY,
+      SORT_KEY: SORT_KEY
+    };
+
+    const lambdaCode = lambda.Code.fromAsset('lambda');
+
+    const checkinLambda = new lambda.Function(this, 'CheckinLambda', {
+      code: lambdaCode,
       handler: 'heartbeat.checkin',
       runtime: lambda.Runtime.NODEJS_10_X,
-      environment: {
-        TABLE_NAME: table.tableName,
-        PARTITION_KEY: PARTITION_KEY,
-        SORT_KEY: SORT_KEY
-      },
+      environment: lambdaEnvironment,
       tracing: lambda.Tracing.ACTIVE
     });
 
-    table.grantWriteData(checkin);
+    checkinTable.grantWriteData(checkinLambda);
+
+    const backLambda = new lambda.Function(this, 'BackLambda', {
+      code: lambdaCode,
+      handler: 'heartbeat.back',
+      runtime: lambda.Runtime.NODEJS_10_X,
+      environment: lambdaEnvironment,
+      tracing: lambda.Tracing.ACTIVE
+    });
+
+    backTable.grantWriteData(backLambda);
 
     const api = new HttpApi(this, 'Api', {
       apiName: 'heartbeat'
@@ -42,7 +71,14 @@ export class HeartbeatStack extends cdk.Stack {
     api.addRoutes({
       path: "/{reporter}/checkin",
       integration: new LambdaProxyIntegration({
-        handler: checkin
+        handler: checkinLambda
+      })
+    });
+
+    api.addRoutes({
+      path: "/{reporter}/back",
+      integration: new LambdaProxyIntegration({
+        handler: backLambda
       })
     });
   }
