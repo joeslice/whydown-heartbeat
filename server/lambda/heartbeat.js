c222e4d56b594dcd76d1c7b1a9be654dd2c183d1
Joe Pollard <joeslice@gmail.com>
2026-04-14 17:15:11 -0500

    Clean up aws interactions

--- server/lambda/heartbeat.js
+++ server/lambda/heartbeat.js
@@ -1,6 +1,9 @@
-const AWS = require('aws-sdk')
-const db = new AWS.DynamoDB.DocumentClient();
-const sns = new AWS.SNS();
+const { DynamoDBClient } = require('@aws-sdk/client-dynamodb');
+const { DynamoDBDocumentClient, UpdateCommand, PutCommand, GetCommand, QueryCommand } = require('@aws-sdk/lib-dynamodb');
+const { SNSClient, PublishCommand } = require('@aws-sdk/client-sns');
+
+const db = DynamoDBDocumentClient.from(new DynamoDBClient());
+const sns = new SNSClient();
 
 const checkinTable = process.env.CHECKIN_TABLE_NAME;
 const outageTable = process.env.OUTAGE_TABLE_NAME;
@@ -11,91 +14,75 @@ exports.checkin = async function(event) {
     const { queryStringParameters, pathParameters } = event;
 
     if (queryStringParameters?.['pingId'] && pathParameters?.['reporter']) {
-        const { pingId, outage, missed} = queryStringParameters;
+        const { pingId, outage, missed } = queryStringParameters;
         const { reporter } = pathParameters;
-        
-        updates = [
-            db.update({
+
+        const updates = [
+            db.send(new UpdateCommand({
                 TableName: checkinTable,
-                Key: {
-                    reporter: reporter
-                },
+                Key: { reporter },
                 UpdateExpression: `set lastPing = :pingId , checkinTime = :checkinTime`,
                 ExpressionAttributeValues: {
                     ':pingId': pingId,
-                    ':checkinTime': Date.now()/1000
+                    ':checkinTime': Date.now() / 1000
                 }
-            }).promise()
+            }))
         ];
 
         if (outage || missed) {
             updates.push(
-                db.put({
+                db.send(new PutCommand({
                     TableName: outageTable,
-                    Item: {
-                        reporter: reporter,
-                        checkinTime: Date.now()/1000,
-                        outage: outage,
-                        missed: missed
-                    }
-                }).promise()
+                    Item: { reporter, checkinTime: Date.now() / 1000, outage, missed }
+                }))
             );
             updates.push(
-                sns.publish({
-                    Message: JSON.stringify({
-                        reporter: reporter,
-                        outage: outage,
-                        missed: missed
-                    }),
+                sns.send(new PublishCommand({
+                    Message: JSON.stringify({ reporter, outage, missed }),
                     TopicArn: snsTopic
-                }).promise()
+                }))
             );
         }
 
         await Promise.all(updates);
-        
+
         return { statusCode: 204, body: '' };
     } else {
-        return { statusCode: 400 }
+        return { statusCode: 400 };
     }
 };
 
 exports.query = async function(event) {
     console.log("request", JSON.stringify(event, undefined, 2));
     const { pathParameters } = event;
+
     if (pathParameters?.['reporter']) {
         const { reporter } = pathParameters;
 
-        const checkinPromise = db.get({
-            TableName: checkinTable,
-            Key: {
-                reporter: reporter
-            }
-        }).promise();
+        const [checkinResult, outageResult] = await Promise.all([
+            db.send(new GetCommand({
+                TableName: checkinTable,
+                Key: { reporter }
+            })),
+            db.send(new QueryCommand({
+                TableName: outageTable,
+                KeyConditionExpression: "#reporter = :reporter",
+                ExpressionAttributeNames: { "#reporter": "reporter" },
+                ExpressionAttributeValues: { ":reporter": reporter }
+            }))
+        ]);
 
-        const outagePromise = db.query({
-            TableName: outageTable,
-            KeyConditionExpression: "#reporter = :rpter",
-            ExpressionAttributeNames: {
-                "#reporter": "reporter"
-            },
-            ExpressionAttributeValues: {
-                ":rpter": reporter
-            }
-        }).promise();
-
-        const results = await Promise.all([checkinPromise, outagePromise]);
-        console.log("results", JSON.stringify(results, undefined, 2));
+        console.log("results", JSON.stringify([checkinResult, outageResult], undefined, 2));
 
         return {
             statusCode: 200,
             body: JSON.stringify({
-                checkin: results[0].Item,
-                outages: results[1].Items,
-                outageCount: results[1].Count
+                checkin: checkinResult.Item,
+                outages: outageResult.Items,
+                outageCount: outageResult.Count
             })
         };
     } else {
         return { statusCode: 400 };
     }
-}
+};
